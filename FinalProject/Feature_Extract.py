088809bed711fca94d9d7fd90b406efcbd961bbc
liujunhan <jimliujunhan@sina.cn>
2020-07-26 00:45:12 -0400

    combined approach. SVM to NN (0.865, 9055)

---
+++ FinalProject/Feature_Extract.py
@@ -0,0 +1,283 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from DataSet import DataSet
+from scipy.sparse import hstack,vstack,csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+import os
+import pickle
+from utils import utils
+import re
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from tqdm import tqdm
+from imblearn.over_sampling import SMOTE
+import random
+
+class FeatureExtract(object):
+
+    def __init__(self, data=None, over_sampling=False, class_format=None, set='train'):
+        '''
+
+        :param data: (np.array) has ['headline', 'body id', 'stance'] as rows
+        :param over_sampling: (Boolean)
+            If True, resampling data set using oversampling.
+            Defualt=False
+        :param separate: (String) has the value of [binary, trinary].
+            if binary, means to convert the 4 labels of input data set to 2 labels.
+            change[agree, disagree, discuss] to related, and keep label 'unrelated' untouch
+        :param set: (String) has values [train, test]
+        '''
+        self.__class_format = class_format
+        self.__set = set
+        self.label = {'agree': 0, 'disagree': 1, 'discuss': 2, 'unrelated': 3}
+        self.__data, self.label = self.change_data_format(data, class_format, set)
+        self.__over_sam = over_sampling
+        self.__embedding = os.path.join(os.getcwd(), 'data', "glove.6B.50d.txt")
+        # build inverse dict for data
+        self.__inverse_doc = self.__inverse_doc()
+        # build tf vector for head+body
+        self.__tf_vector = self.data_to_tf(data)
+        # build tf idf word corpus for entire news data set
+        self.__tfidf_lookup = self.tfidf_lookup(np.r_[self.__data[:, 1], self.__data[:, 0]])
+        self.__glove_vectors = self.__load_word_embedding()
+        self.__x_train, self.__y_train = self.__extract_features()
+
+    def change_data_format(self, data, class_format, set):
+        '''
+        input data set has 4 labels, which is [agree, disagree, discuss, unrelated].
+        Given class_format, change 4 labels to 2 or 3.
+        data set with 2 labels is for binary classification, to be fed into SVM classifier(or you can
+        change the classifier in main.py).
+        Data with 3 labels is to be fed into neural network model.
+
+        :param data: (ndarray) [[head,body,stance]], has labels [agree, disagree, discuss, unrelated].
+        :param class_format: (String) has values [binary, trinary]
+            indicates how many labels left after changing.
+            if binary, change [agree, disagree, discuss] to related. keep unrelated untouch.
+            if trinary, delete rows with label unrelated. keep only [agree, disagree, discuss] for classification.
+        :param set: (String) has values [train, test].
+            if train, the input data set needs to be changed to binary or trinary format.
+            if test, the input data set remains the same.
+            i.e. when trainning, the binary training set has 25413 rows with 2 labels. Trinary set has less rows with
+            3 labels. When testing, the data set remains 25413 rows and row order unchanged.
+        :return: (ndarrray) new data set with 2 or 3 labels.
+        '''
+
+        if set == 'train':
+            if class_format == 'binary':
+                # data set to feed conventional classifier
+                new_data = self.to_binary_labels(data)
+                label = {'related': 0, 'unrelated': 1}
+            if class_format == 'trinary':
+                # data set to feed neural network
+                label = {'agree': 0, 'disagree': 1, 'discuss': 2}
+                new_data = self.to_trinary_labels(data)
+        else:
+            new_data = data
+            label = {'agree': 0, 'disagree': 1, 'discuss': 2, 'unrelated': 3}
+
+        return new_data, label
+
+    def to_binary_labels(self, data):
+        '''
+        convert the original data set, change labels [agree, disagree, discuss] to [related]
+        for conventional classifier to train. keep unrelated label untouched
+
+        :param data: (ndarray) [[head,body,stance]], has labels [agree, disagree, discuss, unrelated].
+        :return: (ndarray) [[head,body,stance]],
+        '''
+        new_data = pd.DataFrame(data, columns=['head','body','stance'])
+        new_data.loc[new_data['stance']=='discuss', 'stance'] = 'related'
+        new_data.loc[new_data['stance'] == 'agree', 'stance'] = 'related'
+        new_data.loc[new_data['stance'] == 'disagree', 'stance'] = 'related'
+        # shuffle set
+        new_data = new_data.sample(frac=1)
+        # keep the order of index after shuffle
+        index = new_data[:].index.tolist()
+        new_data = np.array(new_data)
+        return new_data
+
+    def to_trinary_labels(self, data):
+        '''
+        select a subset from original data set. choose rows that have label
+        of [agree, disagree, discuss] for neural network to train.
+
+        :param data: (ndarray) [[head,body,stance]], has labels [agree, disagree, discuss, unrelated].
+        :return: (ndarray) [[head,body,stance]],
+        '''
+        new_data = pd.DataFrame(data, columns=['head','body','stance'])
+        new_data = pd.concat([new_data[new_data['stance']=='agree'], new_data[new_data['stance']=='disagree'], new_data[new_data['stance']=='discuss']])
+        # shuffle
+        new_data = new_data.sample(frac=1)
+        # keep the order of rows after shuffle
+        index = new_data[:].index.tolist()
+        new_data = np.array(new_data)
+        return new_data
+
+    def __extract_features(self):
+        '''
+        using self.__data's [head, body] to extract features.
+
+        :return: (ndarray), (ndarray)
+        '''
+
+        # if trinary, means using neural network to classify [agree, disagree, discuss] using 2 features
+        if self.__class_format == 'trinary':
+            ftrs = [self.cosine_similarity, self.ngram_overlap]
+        else:
+            ftrs = [self.cosine_similarity, self.ngram_overlap]
+
+        x_train = []
+        for doc in tqdm(self.__data, desc="[Extracting features: ]"):
+            vec = np.array([0.0] * len(ftrs))
+            for i in range(len(ftrs)):
+                vec[i] = ftrs[i](doc)
+            x_train.append(vec)
+        x_train = np.array(x_train)
+        y_train = np.array([self.label[i] for i in self.__data[:, 2]])
+
+        if(self.__over_sam):
+            print("Over-sampling...")
+            sm = SMOTE(random_state=0)
+            x_train, y_train = sm.fit_resample(x_train, y_train)
+
+        return x_train, y_train
+
+    def __inverse_doc(self):
+        '''
+        build a inverse lookup dict for [[head, body, stance]].
+        each item [head,body,stance] is the key. row index is the value
+
+        :return: (Dict)
+        '''
+
+        # build inverse dict for data, key is [head, body, stance], value is row index
+        dict = {}
+        for i in tqdm(range(len(self.__data)), desc="[Building inverse doc: ]"):
+            dict[self.__data[i].tobytes()] = i
+        return  dict
+
+    def data_to_tf(self, doc):
+        '''
+        Build tf vectors for doc. Combine head+body together and using vector of size 5000 to represent it.
+
+        :param doc: (ndarray) [[head, body, stance]].
+        :return: Tf-idf-weighted document-term matrix
+        '''
+
+        # row[0] is head, row[1] is body
+        data = [row[0]+" "+row[1] for row in tqdm(doc, desc="[Building tf vectors: ]")]
+        tf_vectorizer = TfidfVectorizer(use_idf=False, token_pattern=r"(?u)\b\w+\b", max_features=5000)
+        tf = tf_vectorizer.fit_transform(data).toarray()
+
+        return tf
+
+    def tokenise(self, text):
+        pattern = re.compile("[^a-zA-Z0-9 ]+")
+        stop_words = set(stopwords.words('english'))
+        text = pattern.sub('', text.replace('\n', ' ').replace('-', ' ').lower())
+        text = [word for word in word_tokenize(text) if word not in stop_words]
+        return text
+
+    def doc_to_tf(self, text, ngram=1):
+        words = self.tokenise(text)
+        ret = defaultdict(float)
+        for i in range(len(words)):
+            for j in range(1, ngram + 1):
+                if i - j < 0:
+                    break
+                word = [words[i - k] for k in range(j)]
+                ret[word[0] if ngram == 1 else tuple(word)] += 1.0
+        return ret
+
+    def tfidf_lookup(self, doc):
+        '''
+        build a tf-idf lookup table for distinct words in doc.
+
+        :param doc: iterable
+            An iterable which yields either str, unicode or file objects.
+        :return: (Dict)
+            tf-idf lookup dictionary{word, tf-idf}
+        '''
+        print("Building tf-idf vectors...")
+        data = [' '.join(self.tokenise(sentence)) for sentence in doc]
+
+        # build tf-idf lookup table, token_pattern keeps length of 1 words
+        tfidf_vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True, token_pattern=r"(?u)\b\w+\b")
+        tfidf = tfidf_vectorizer.fit_transform(data)
+        coo_matrix = tfidf.tocoo()
+        tuples = zip(coo_matrix.col, coo_matrix.data)
+        sorted_items = sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+        feature_names =tfidf_vectorizer.get_feature_names()
+
+        word_idf = {}
+        # word index and corresponding tf-idf score
+        for idx, score in tqdm(sorted_items, desc="[Building tf-idf lookup table: ]"):
+            word_idf[feature_names[idx]] = round(score, 3)
+        # return a sorted list of tuples with feature name and tf-idf score as its element(in descending order of tf-idf scores).
+        return word_idf
+
+    def __load_word_embedding(self):
+        # Load GLoVe word vectors
+        # download from https://nlp.stanford.edu/projects/glove/
+        f_glove = open(self.__embedding, "rb")
+        glove_vectors = {}
+        for line in tqdm(f_glove, desc="[Loading in word embeddings: ]"):
+            glove_vectors[str(line.split()[0]).split("'")[1]] = np.array(list(map(float, line.split()[1:])))
+
+        return glove_vectors
+
+    def doc_to_glove(self, doc):
+        '''
+        Convert a document to GloVe vectors, by computing tf-idf of each word * GLoVe of word / total tf-idf for document.
+        project each word token in doc onto GLoVe word embedding vectors using tf-idf of the word.
+
+        :param doc: (String) string of words.
+        :return: (ndarray)
+        '''
+
+        words_lst = self.tokenise(doc)
+        # initialize vector of length as length of word embedding
+        doc_vector = np.zeros(self.__glove_vectors['glove'].shape[0])
+        if np.sum(list(self.__tfidf_lookup.values())) == 0.0:  # edge case: document is empty
+            return doc_vector
+
+        for word in words_lst:
+            if word in self.__glove_vectors and word in self.__tfidf_lookup:
+                doc_vector += self.__glove_vectors[word] * self.__tfidf_lookup[word]
+        doc_vector /= np.sum(list(self.__tfidf_lookup.values()))
+        return doc_vector
+
+    def cosine_similarity(self, doc):
+        '''
+
+        :param doc: (ndarray) [head,body,stance]
+        :return: (Float)
+        '''
+        headline_vector = self.doc_to_glove(doc[0])
+        body_vector = self.doc_to_glove(doc[1])
+        cos = cosine_similarity([headline_vector], [body_vector])[0][0]
+        return cos
+
+    def ngram_overlap(self, doc):
+        tf_headline = self.doc_to_tf(doc[0], ngram=2)
+        tf_body = self.doc_to_tf(doc[1], ngram=2)
+        matches = 0.0
+        for words in tf_headline.keys():
+            if words in tf_body:
+                matches += tf_body[words]
+        return np.power((matches / len(self.tokenise(doc[1]))), 1 / np.e)
+
+    def get_X(self):
+        return self.__x_train
+
+    def get_y(self):
+        return self.__y_train
+
+    def get_tfidf_lookup(self):
+        return self.__tfidf_lookup
+
+
---
+++ FinalProject/main.py
@@ -0,0 +1,244 @@
+import csv
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import re
+from collections import defaultdict
+from scorer import score_submission, print_confusion_matrix, score_defaults, SCORE_REPORT
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+import os
+from imblearn.over_sampling import SMOTE
+from DataSet import DataSet
+from Feature_Extract import FeatureExtract
+from utils import utils, SUBMISSION_PATH, MODEL_PATH, DATA_PATH
+from sklearn.svm import SVC
+from scorer import score_submission, print_confusion_matrix, score_defaults, SCORE_REPORT, load_dataset
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from Preprocess import Preprocess
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras import initializers
+from keras.layers import Dense, Dropout
+from keras.layers import Flatten
+from keras.layers import Embedding, LSTM, GRU, GlobalMaxPool1D
+from keras.utils import np_utils
+from tensorflow.keras import regularizers
+from sklearn.metrics import accuracy_score
+from keras.utils import np_utils
+from sklearn.ensemble import GradientBoostingClassifier
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+# CONFIGURATION PARAMETER
+PREPROCESS = False
+OVER_SAMPLING = False
+TF_MAX_FEATURE = 5000
+
+class main():
+
+    def __init__(self, path=None):
+        self.path = path
+        self.__classifiers = self.classifiers()
+        self.__model_score = {}
+        # store predictions of one conventional classifier like SVM in self.__classifiers list
+        # the order of the predictions is the same order of testing set
+        self.half_pred = []
+
+
+    def classify(self):
+        '''
+
+        :return:
+        '''
+
+        "prepare data"
+        utils.print_info("1, prepareing data")
+        ds = DataSet(preprocess=PREPROCESS)
+        train_all = ds.get_train()
+        val_all = ds.get_validation()
+        test_all = ds.get_test()
+
+        "extract features from training set"
+        utils.print_info("2, extracting training features")
+        # using NN model to train, 2 features with labels [agree, disagree, discuss]
+        fe = FeatureExtract(train_all, over_sampling=OVER_SAMPLING, set='train', class_format='trinary')
+        X_train_tri, y_train_tri = fe.get_X(), fe.get_y()
+        # using SVM to train, 3 features, with label [related, unrelated]
+        fe_bi = FeatureExtract(train_all, over_sampling=OVER_SAMPLING, set='train', class_format='binary')
+        X_train_bi, y_train_bi = fe_bi.get_X(), fe_bi.get_y()
+
+        "extract features from testing set"
+        utils.print_info("3, extracting testing features")
+        # using NN model to test, 2 features
+        fe_test = FeatureExtract(test_all, set='test', class_format='trinary')
+        X_test, y_test = fe_test.get_X(), fe_test.get_y()
+        # using SVM to predict
+        fe_test_bi = FeatureExtract(test_all, set='test', class_format='binary')
+        X_test_bi, y_test_bi = fe_test_bi.get_X(), fe_test_bi.get_y()
+
+        "fit model"
+        utils.print_info("4, fitting/testing model")
+        self.clf_comparison(X_train_bi, y_train_bi, X_test_bi, y_test_bi)
+
+        "testing"
+        utils.print_info("5, neural network model")
+        self.NN_model(X_train_tri,y_train_tri,X_test,y_test)
+
+    def classifiers(self):
+        return [
+            (SVC(C=50.0, cache_size=200, class_weight=None, coef0=0.0,
+                 decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+                 max_iter=-1, probability=True, random_state=None, shrinking=True,
+                 tol=0.001, verbose=False), 'SVM', 'SVM'),
+
+            # (GradientBoostingClassifier(random_state=0), 'GBDT', 'GBDT'),
+            #
+            # (KNeighborsClassifier(1), "K NN ", 'KNN'),
+            #
+            # (QuadraticDiscriminantAnalysis(), 'Qudratic Discriminant Analysis', 'QD'),
+            #
+            # (RandomForestClassifier(max_depth=50, n_estimators=10, max_features=1), 'Random Forest Classifier', 'RF'),
+            #
+            # (AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=0.01,
+            #                     algorithm='SAMME.R', random_state=None), 'Adaboost Classifier', 'Ada'),
+            # (SGDClassifier(), 'SGD Classifier', 'SGD'),
+            #
+            # (DecisionTreeClassifier(max_depth=5), 'Decision Tree Classifier', 'DT'),
+            #
+            # (LinearDiscriminantAnalysis(solver='svd', shrinkage=None, priors=None, n_components=None,
+            #                             store_covariance=False, tol=0.00001), 'Linear Discriminant Analysis', 'LDA'),
+            #
+            # (GaussianNB(), 'Gaussian Naive Bayes ', 'GNB')
+        ]
+
+    def clf_comparison(self, X, y, X_test, y_test):
+        '''
+        Frist step of 2 step classification.
+        using conventional classifier to train data set with only labels [related, unrelated].
+        By using SVM, the accuracy of binary classification can be above 95%.
+        store the predition result in global variable for future use.
+
+        :param X: x train
+        :param y: y train
+        :param X_test:
+        :param y_test:
+        :return:
+        '''
+
+        competition_test = utils.read('competition_test_stances.csv')
+        gold_labels = load_dataset(os.path.join(DATA_PATH, 'competition_test_stances.csv'))
+
+        for model, name, initial in self.__classifiers:
+            print("Fitting...")
+            clf = model.fit(X, y)
+            print("Predicting...")
+            predictions = clf.predict(X_test)
+            # store GDBT's prediction on agree and disagree
+            self.half_pred = predictions
+            print(name, " Testing score: ", clf.score(X_test, y_test))
+            # save model
+            utils.save_pkl_model(clf, initial)
+
+    def NN_model(self, X, y, X_test, y_test):
+        '''
+        second step of 2 step classification.
+        using neural network to classify 3 labels [agree, disagree, discuss].
+
+        :param X:
+        :param y:
+        :param X_test:
+        :param y_test:
+        :return:
+        '''
+        y = np_utils.to_categorical(y, 3)
+        y_true = y_test
+        # y_test = np_utils.to_categorical(y_test, 3)
+
+        init = initializers.glorot_uniform(seed=1)
+        model = Sequential()
+        model.add(Dense(units=20, input_dim=2,kernel_initializer=init,  activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(units=10, kernel_initializer=init, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(units=3, kernel_initializer=init, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+        model.summary()
+        model.fit(X, y, batch_size=5, epochs=50, verbose=2)
+        # loss, accuracy = model.evaluate(X_test, y_test)
+        # print('Accuracy: %f' % (accuracy * 100))
+        # exit()
+        predictions = np.argmax(model.predict(X_test), axis=-1)
+
+        "combine clf and NN clf's result together"
+        predictions = self.__combine(self.half_pred, predictions)
+
+        print(accuracy_score(y_true, predictions))
+
+        "save the model"
+        model.save(os.path.join(os.getcwd(), 'model','FNN.h5'))
+
+        "write out submission file"
+        utils.print_info("5, Writing submission results")
+        competition_test = utils.read('competition_test_stances.csv')
+        print(len(competition_test), len(predictions))
+        utils.write_out(competition_test, predictions, 'answer')
+
+        "print confusion matrix and score"
+        gold_labels = load_dataset(os.path.join(DATA_PATH, 'competition_test_stances.csv'))
+        test_labels = load_dataset(os.path.join(SUBMISSION_PATH, 'answer.csv'))
+        test_score, cm = score_submission(gold_labels, test_labels)
+        null_score, max_score = score_defaults(gold_labels)
+        print_confusion_matrix(cm)
+        print(SCORE_REPORT.format(max_score, null_score, test_score))
+        self.__model_score['FNN'] = [test_score, print_confusion_matrix(cm)]
+
+    def __combine(self, clf_pred, nn_pred):
+        '''
+        clf_pred contains prediction of 2 labels [0,1] which 0 is related, 1 is unrelated.
+        nn_pred contains prediction of 3 labels [0,1,2] which 0 is agree, 1 is disagree, 2 is discuss.
+        Combine two predictions together, keep classified label 'unrelated' untouched, and for 'related',
+        using predictions in nn_pred to swap.
+
+        :param clf_pred: (ndarray)
+        :param nn_pred: (ndarray)
+        :return: (ndarray)
+        '''
+        predictions = []
+        for i in range(len(clf_pred)):
+            # 如果是1,代表unrelated, 变成3
+            if clf_pred[i] == 1:
+                predictions.append(3)
+            else:
+                predictions.append(nn_pred[i])
+
+        return np.array(predictions)
+
+
+if __name__ == '__main__':
+    "before running, download word_embedding file at https://nlp.stanford.edu/projects/glove/"
+    "https://www.kaggle.com/pkugoodspeed/nlpword2vecembeddingspretrained?select=glove.6B.50d.txt"
+    "put the glove.6B.50d.txt into data folder and you are good to go"
+    main().classify()
+
+
+
+
+
+
